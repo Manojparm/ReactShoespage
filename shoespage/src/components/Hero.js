97bef80dfd867e972aaa924ad37eb82a635445aa
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-07-21 13:34:57 +0530

    completed my project

---
+++ shoespage/src/components/Hero.js
@@ -0,0 +1,24 @@
+const Hero=()=>{
+    return (
+        <main className="section">
+             <div className="text">
+                <h1>FEELS LIKE <br />NOTHING ELSE</h1>
+                <p>"I've never experienced such comfort while
+                   running.<br/> It's like my feet found their soulmate!"</p>
+                   <main className="button">
+                    <button className="one">Shop Now</button>
+                    <button className="two">Category</button>
+                   </main>
+                   <p>Available on</p>
+                   <div className="shoplogo">
+                     <h1>Amazone</h1>
+                   </div>
+                 </div>
+                 <div className="brand-img">
+                 <img src=".\images\brand shoes.jpg" alt="" />
+                 </div>
+        </main>
+    )
+}
+
+export default Hero;
---
+++ shoespage/src/components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar=()=>{
+    return (
+     <nav>
+        <div className="logog">
+            <h1><span>My</span>shoes</h1>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Contact</li>
+        </ul>
+        <button className="nav">Singup</button>
+     </nav>
+    )
+}
+
+export default Navbar;
